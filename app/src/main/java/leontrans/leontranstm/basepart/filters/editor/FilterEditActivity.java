923afaffe0dca6a71173e883767d6e024016dfac
Ar-Krav <ar-krav@meta.ua>
2017-12-17 20:08:29 +0200

    Filter editing activity done

--- app/src/main/java/leontrans/leontranstm/basepart/filters/editor/FilterEditActivity.java
+++ app/src/main/java/leontrans/leontranstm/basepart/filters/editor/FilterEditActivity.java
@@ -6,18 +6,19 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import leontrans.leontranstm.R;
 import leontrans.leontranstm.basepart.filters.FilterSettingsActivity;
@@ -32,9 +33,9 @@ public class FilterEditActivity extends AppCompatActivity {
     private Toolbar toolbar;
     String notifyId;
 
-    Spinner notifyTypeSpinenr;
-    Spinner carTypeSpinenr;
-    Spinner carKindSpinenr;
+    Spinner notifyTypeSpinner;
+    Spinner carTypeSpinner;
+    Spinner carKindSpinner;
 
     ArrayList<String> docsArrayList;
     ArrayList<String> loadTypeArrayList;
@@ -49,9 +50,9 @@ public class FilterEditActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        notifyTypeSpinenr = (Spinner) findViewById(R.id.notify_type_spinner);
-        carTypeSpinenr = (Spinner) findViewById(R.id.car_type);
-        carKindSpinenr = (Spinner) findViewById(R.id.car_kind);
+        notifyTypeSpinner = (Spinner) findViewById(R.id.notify_type_spinner);
+        carTypeSpinner = (Spinner) findViewById(R.id.car_type);
+        carKindSpinner = (Spinner) findViewById(R.id.car_kind);
 
         ArrayAdapter<?> notifyTypeAdapter = ArrayAdapter.createFromResource(this, R.array.notify_types, android.R.layout.simple_spinner_item);
             notifyTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -60,9 +61,9 @@ public class FilterEditActivity extends AppCompatActivity {
         ArrayAdapter<?> carKindAdapter = ArrayAdapter.createFromResource(this, R.array.car_kind, android.R.layout.simple_spinner_item);
             carKindAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        notifyTypeSpinenr.setAdapter(notifyTypeAdapter);
-        carTypeSpinenr.setAdapter(carTypeAdapter);
-        carKindSpinenr.setAdapter(carKindAdapter);
+        notifyTypeSpinner.setAdapter(notifyTypeAdapter);
+        carTypeSpinner.setAdapter(carTypeAdapter);
+        carKindSpinner.setAdapter(carKindAdapter);
 
         docsArrayList = new ArrayList<>();
         loadTypeArrayList = new ArrayList<>();
@@ -95,11 +96,33 @@ public class FilterEditActivity extends AppCompatActivity {
             }
         });
 
+        ((Button) findViewById(R.id.save_button)).setOnClickListener(getSaveBtnClickListener());
+        ((Button) findViewById(R.id.cancel_button)).setOnClickListener(getCancelBtnClickListener());
 
         notifyId = getIntent().getStringExtra("notifyId");
         new LoadFilterInfo().execute();
     }
 
+    private View.OnClickListener getSaveBtnClickListener(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO send all result to server
+                Intent intent = new Intent(FilterEditActivity.this, FilterSettingsActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener getCancelBtnClickListener(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FilterEditActivity.this, FilterSettingsActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
 
     private class LoadFilterInfo extends AsyncTask<Void,Void,String> {
 
@@ -160,149 +183,149 @@ public class FilterEditActivity extends AppCompatActivity {
         private void setNotifySpinnerSelection(String notifyType){
             switch (notifyType){
                 case "": {
-                    notifyTypeSpinenr.setSelection(0);
+                    notifyTypeSpinner.setSelection(0);
                     break;
                 }
                 case "car": {
-                    notifyTypeSpinenr.setSelection(1);
+                    notifyTypeSpinner.setSelection(1);
                     break;
                 }case "luggage": {
-                    notifyTypeSpinenr.setSelection(2);
+                    notifyTypeSpinner.setSelection(2);
                     break;
                 }
-                default: notifyTypeSpinenr.setSelection(0);
+                default: notifyTypeSpinner.setSelection(0);
             }
         }
 
         private void setCarTypeSpinnerSelection(String carType){
             switch (carType){
                 case "":{
-                    carTypeSpinenr.setSelection(0);
+                    carTypeSpinner.setSelection(0);
                     break;
                 }
                 case "any":{
-                    carTypeSpinenr.setSelection(1);
+                    carTypeSpinner.setSelection(1);
                     break;
                 }
                 case "bus":{
-                    carTypeSpinenr.setSelection(2);
+                    carTypeSpinner.setSelection(2);
                     break;
                 }
                 case "avto":{
-                    carTypeSpinenr.setSelection(3);
+                    carTypeSpinner.setSelection(3);
                     break;
                 }
                 case "fuel_oil":{
-                    carTypeSpinenr.setSelection(4);
+                    carTypeSpinner.setSelection(4);
                     break;
                 }
                 case "concrete":{
-                    carTypeSpinenr.setSelection(5);
+                    carTypeSpinner.setSelection(5);
                     break;
                 }
                 case "gas":{
-                    carTypeSpinenr.setSelection(6);
+                    carTypeSpinner.setSelection(6);
                     break;
                 }
                 case "hard":{
-                    carTypeSpinenr.setSelection(7);
+                    carTypeSpinner.setSelection(7);
                     break;
                 }
                 case "grain":{
-                    carTypeSpinenr.setSelection(8);
+                    carTypeSpinner.setSelection(8);
                     break;
                 }
                 case "isotherms":{
-                    carTypeSpinenr.setSelection(9);
+                    carTypeSpinner.setSelection(9);
                     break;
                 }
                 case "containertrans":{
-                    carTypeSpinenr.setSelection(10);
+                    carTypeSpinner.setSelection(10);
                     break;
                 }
                 case "tap":{
-                    carTypeSpinenr.setSelection(11);
+                    carTypeSpinner.setSelection(11);
                     break;
                 }
                 case "closed":{
-                    carTypeSpinenr.setSelection(12);
+                    carTypeSpinner.setSelection(12);
                     break;
                 }
                 case "trees":{
-                    carTypeSpinenr.setSelection(13);
+                    carTypeSpinner.setSelection(13);
                     break;
                 }
                 case "microbus":{
-                    carTypeSpinenr.setSelection(1);
+                    carTypeSpinner.setSelection(1);
                     break;
                 }
                 case "oversized":{
-                    carTypeSpinenr.setSelection(14);
+                    carTypeSpinner.setSelection(14);
                     break;
                 }
                 case "unclosed":{
-                    carTypeSpinenr.setSelection(15);
+                    carTypeSpinner.setSelection(15);
                     break;
                 }
                 case "refrigerator":{
-                    carTypeSpinenr.setSelection(16);
+                    carTypeSpinner.setSelection(16);
                     break;
                 }
                 case "tipper":{
-                    carTypeSpinenr.setSelection(17);
+                    carTypeSpinner.setSelection(17);
                     break;
                 }
                 case "animaltruck":{
-                    carTypeSpinenr.setSelection(18);
+                    carTypeSpinner.setSelection(18);
                     break;
                 }
                 case "awning":{
-                    carTypeSpinenr.setSelection(19);
+                    carTypeSpinner.setSelection(19);
                     break;
                 }
                 case "trall":{
-                    carTypeSpinenr.setSelection(20);
+                    carTypeSpinner.setSelection(20);
                     break;
                 }
                 case "avtotipper":{
-                    carTypeSpinenr.setSelection(21);
+                    carTypeSpinner.setSelection(21);
                     break;
                 }
                 case "fullmetal":{
-                    carTypeSpinenr.setSelection(22);
+                    carTypeSpinner.setSelection(22);
                     break;
                 }
                 case "fuel_oil_small":{
-                    carTypeSpinenr.setSelection(23);
+                    carTypeSpinner.setSelection(23);
                     break;
                 }
                 case "evacuator":{
-                    carTypeSpinenr.setSelection(24);
+                    carTypeSpinner.setSelection(24);
                     break;
                 }
-                default: carTypeSpinenr.setSelection(0);
+                default: carTypeSpinner.setSelection(0);
             }
         }
 
         private void setCarKindSpinnerSelection(String carKind){
             switch (carKind){
                 case "":{
-                    carKindSpinenr.setSelection(0);
+                    carKindSpinner.setSelection(0);
                     break;
                 }
                 case "truck":{
-                    carKindSpinenr.setSelection(1);
+                    carKindSpinner.setSelection(1);
                     break;
                 }
                 case "trailer":{
-                    carKindSpinenr.setSelection(2);
+                    carKindSpinner.setSelection(2);
                     break;
                 }
                 case "half-trailer":{
-                    carKindSpinenr.setSelection(3);
+                    carKindSpinner.setSelection(3);
                     break;
                 }
-                default: carKindSpinenr.setSelection(0);
+                default: carKindSpinner.setSelection(0);
             }
         }
     }
